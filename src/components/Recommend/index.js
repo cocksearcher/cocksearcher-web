7b07f00b173c3de97426a832ed321fb6a48111e4
limyeonsang <limquo@gmail.com>
2022-05-03 08:59:02 +0900

    Add: params

--- src/components/Recommend/index.js
+++ src/components/Recommend/index.js
@@ -2,25 +2,33 @@ import { useEffect } from "react";
 import { Instance } from "../../axios";
 
 const Recommend = ({ countEmotion }) => {
-  const a = countEmotion.map((element) => {
-    element.
-  })  
-
   useEffect(() => {
     const req = async () => {
-      const res = await Instance.get("/cocktail/today", {
-        angry: 3,
-        exciting: 4,
+      const emotionObj = {
         happy: 1,
+        exciting: 4,
         hungry: 2,
         sad: 6,
-      });
+        angry: 3,
+      };
+
+      const params = Object.keys(emotionObj).map(
+        (key, idx) => (emotionObj[key] = countEmotion[idx].count)
+      );
+
+      const res = await Instance.get("/cocktail/today", params);
 
       console.log(res.data);
     };
 
     req();
-  }, []);
+  }, [countEmotion]);
+
+  return  (
+    <div>
+      <div>sfjksfhjksebfjksb</div>
+    </div>
+  )
 };
 
 export default Recommend;
